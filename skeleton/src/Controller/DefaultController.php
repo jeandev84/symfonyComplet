284b3fad8ad41f10454e649af798d18937d716ec
Yao Jean-Claude <jeandev84@yahoo.com>
2019-12-18 15:29:55 +0300

    ManyToMany relationship

--- skeleton/src/Controller/DefaultController.php
+++ skeleton/src/Controller/DefaultController.php
@@ -32,22 +32,6 @@ class DefaultController extends AbstractController
     {
        $entityManager = $this->getDoctrine()->getManager();
 
-       $user = new User();
-       $user->setName('John');
-
-       $address = new Address();
-       $address->setStreet('street');
-       $address->setNumber(23);
-       $user->setAddress($address);
-
-       $entityManager->persist($user);
-       $entityManager->persist($address); // required, if `cascade: persist` is not set
-
-       $entityManager->flush();
-
-
-       dump($user->getAddress()->getStreet());
-
        return $this->render('default/index.html.twig', [
            'controller_name' => 'DefaultController'
        ]);
